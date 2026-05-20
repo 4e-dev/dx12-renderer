4b9d01fe35ba80ece54200c4149792946074fe8e
bao <80117447+4e-dev@users.noreply.github.com>
2026-05-20 13:13:49 -0400

    Create skeleton for GameTimer

---
+++ Engine/Core/GameTimer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class GameTimer
+{
+public:
+    GameTimer();
+
+    float GameTime() const;
+    float DeltaTime() const;
+
+    void Reset();
+    void Start();
+    void Stop();
+    void Tick();
+
+private:
+    double secondsPerCount;
+    double deltaTime;
+
+    __int64 baseTime;
+    __int64 pausedTime;
+    __int64 stopTime;
+    __int64 previousTime;
+    __int64 currentTime;
+
+    bool stopped;
+};
