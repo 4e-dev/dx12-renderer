016e3562bbefc174e5ee1d0403a7a272a7bb95cf
bao <80117447+4e-dev@users.noreply.github.com>
2026-05-20 13:19:41 -0400

    Define constructor for GameTimer

--- Engine/Core/GameTimer.cpp
+++ Engine/Core/GameTimer.cpp
@@ -1,1 +1,19 @@
 #include "GameTimer.h"
+
+GameTimer::GameTimer()
+{
+    // Initialize values
+    secondsPerCount     =  0.0;
+    deltaTime           = -1.0;
+    baseTime            = 0;
+    pausedTime          = 0;
+    stopTime            = 0;
+    previousTime        = 0;
+    currentTime         = 0;
+    stopped             = false;
+
+    // Calculate the number of seconds that happen per 'Count'
+    __int64 countsPerSecond;
+    QueryPerformanceCounter((LARGE_INTEGER*)&countsPerSecond);
+    secondsPerCount = 1.0 / (double)countsPerSecond;
+}
