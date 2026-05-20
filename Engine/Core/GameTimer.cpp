bac4b2cb861ce0c3b244c1444fd66104ca9da86e
bao <80117447+4e-dev@users.noreply.github.com>
2026-05-20 13:36:07 -0400

    Explicitly mark as member variables of Gametimer

--- Engine/Core/GameTimer.cpp
+++ Engine/Core/GameTimer.cpp
@@ -3,17 +3,53 @@
 GameTimer::GameTimer()
 {
     // Initialize values
-    secondsPerCount     =  0.0;
-    deltaTime           = -1.0;
-    baseTime            = 0;
-    pausedTime          = 0;
-    stopTime            = 0;
-    previousTime        = 0;
-    currentTime         = 0;
-    stopped             = false;
+    mSecondsPerCount     =  0.0;
+    mDeltaTime           = -1.0;
+    mBaseTime            = 0;
+    mPausedTime          = 0;
+    mStopTime            = 0;
+    mPreviousTime        = 0;
+    mCurrentTime         = 0;
+    mStopped             = false;
 
     // Calculate the number of seconds that happen per 'Count'
     __int64 countsPerSecond;
     QueryPerformanceCounter((LARGE_INTEGER*)&countsPerSecond);
-    secondsPerCount = 1.0 / (double)countsPerSecond;
+    mSecondsPerCount = 1.0 / (double)countsPerSecond;
+}
+
+float GameTimer::GameTime() const { return -1.0; }
+
+float GameTimer::DeltaTime() const
+{
+    return (float)mDeltaTime;
+}
+
+void GameTimer::Reset() {}
+
+void GameTimer::Start() {}
+
+void GameTimer::Stop() {}
+
+void GameTimer::Tick()
+{
+    if (mStopped)
+    {
+        mDeltaTime = 0.0;
+        return;
+    }
+
+    // Get time for this frame
+    __int64 _currentTime;
+    QueryPerformanceCounter((LARGE_INTEGER*)&_currentTime);
+    mCurrentTime = _currentTime;
+     
+    // Calculate delta (time between this frame and the previous frame, in seconds)
+    mDeltaTime = (mCurrentTime - mPreviousTime) * mSecondsPerCount;
+
+    // Prepare for next frame
+    mPreviousTime = mCurrentTime;
+
+    // Force nonnegative
+    mDeltaTime = mDeltaTime < 0.0 ? 0.0 : mDeltaTime;
 }
